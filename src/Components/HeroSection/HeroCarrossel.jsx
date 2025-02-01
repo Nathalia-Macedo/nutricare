16442e216e5aa2a03c9c64c22c374c5063d1ce41
Nathalia-Macedo <nathaliademacedomartins04@gmail.com>
2025-02-01 00:54:53 -0300

    subindo correcao nos carrosseis

--- src/Components/HeroSection/HeroCarrossel.jsx
+++ src/Components/HeroSection/HeroCarrossel.jsx
@@ -1,15 +1,119 @@
+// import { useState, useEffect } from "react"
+// import { Swiper, SwiperSlide } from "swiper/react"
+// import { Navigation, Pagination, Autoplay } from "swiper/modules"
+// import "swiper/css"
+// import "swiper/css/navigation"
+// import "swiper/css/pagination"
+// import mobile from '../../Assets/mobile3.png'
+// import desktop2 from '../../Assets/desktop2.png'
+
+// const slides = [
+//   {
+//     id: 1,
+//     desktopImage:
+//       "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Group%20174-Zq5Kg6laYMY4SAqGvRpR4gafN0EXoP.png",
+//     mobileImage:
+//       "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Group%20175-hNOhgoHkZmloIfY9TweXOiEdFWSgG9.png",
+//     targetSection: "about",
+//   },
+//   {
+//     id: 2,
+//     desktopImage:
+//       "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Group%20177-wd2AtgkpxJp2R1C7ed645no4RHAPaZ.png",
+//     mobileImage:
+//       "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Group%20178-qlxmMzTmj9YSWmauHWzxbJCg82S3VN.png",
+//     targetSection: "specialties",
+//   },
+//   {
+//     id: 3,
+//     desktopImage:
+//       desktop2,
+//     mobileImage:
+//       mobile,
+//     targetSection: "team",
+//   },
+//   {
+//     id: 4,
+//     desktopImage:
+//       "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Group%20179-KYso8EiLDRqzfN66Tac9F7rZ5KOgNM.png",
+//     mobileImage:
+//       "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Group%20179%20(1)-XX5SJRUOJcK6el4fLwWIW4vni4pOUK.png",
+//     targetSection: "team",
+//   },
+// ]
+
+// export default function HeroCarousel() {
+//   const [isMobile, setIsMobile] = useState(false)
+
+//   useEffect(() => {
+//     const checkMobile = () => {
+//       setIsMobile(window.innerWidth < 768)
+//     }
+
+//     checkMobile()
+//     window.addEventListener("resize", checkMobile)
+
+//     return () => {
+//       window.removeEventListener("resize", checkMobile)
+//     }
+//   }, [])
+
+//   const scrollToSection = (sectionId) => {
+//     const section = document.getElementById(sectionId)
+//     if (section) {
+//       section.scrollIntoView({ behavior: "smooth" })
+//     }
+//   }
+
+//   return (
+//     <Swiper
+//       modules={[Navigation, Pagination, Autoplay]}
+//       navigation
+//       pagination={{ clickable: true }}
+//       autoplay={{ delay: 5000 }}
+//       className="w-full h-screen"
+//     >
+//       {slides.map((slide) => (
+//         <SwiperSlide key={slide.id}>
+//           <div className="relative w-full h-full cursor-pointer" onClick={() => scrollToSection(slide.targetSection)}>
+//             <img
+//               src={isMobile ? slide.mobileImage : slide.desktopImage}
+//               alt=""
+//               className="w-full h-full object-cover"
+//             />
+//           </div>
+//         </SwiperSlide>
+//       ))}
+//     </Swiper>
+//   )
+// }
+
 import { useState, useEffect } from "react"
 import { Swiper, SwiperSlide } from "swiper/react"
 import { Navigation, Pagination, Autoplay } from "swiper/modules"
 import "swiper/css"
 import "swiper/css/navigation"
 import "swiper/css/pagination"
-import mobile from '../../Assets/mobile3.png'
-import desktop2 from '../../Assets/desktop2.png'
+import mobile from "../../Assets/mobile3.png"
+import desktop2 from "../../Assets/desktop2.png"
 
 const slides = [
   {
     id: 1,
+    desktopImage: desktop2,
+    mobileImage: mobile,
+    targetSection: "team",
+  },
+  {
+    id: 2,
+    desktopImage:
+    "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Group%20179-KYso8EiLDRqzfN66Tac9F7rZ5KOgNM.png",
+  mobileImage:
+    "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Group%20179%20(1)-XX5SJRUOJcK6el4fLwWIW4vni4pOUK.png",
+  targetSection: "team",
+  },
+  {
+    id: 3,
     desktopImage:
       "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Group%20174-Zq5Kg6laYMY4SAqGvRpR4gafN0EXoP.png",
     mobileImage:
@@ -17,28 +121,12 @@ const slides = [
     targetSection: "about",
   },
   {
-    id: 2,
+    id: 4,
     desktopImage:
       "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Group%20177-wd2AtgkpxJp2R1C7ed645no4RHAPaZ.png",
     mobileImage:
       "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Group%20178-qlxmMzTmj9YSWmauHWzxbJCg82S3VN.png",
-    targetSection: "specialties",
-  },
-  {
-    id: 3,
-    desktopImage:
-      desktop2,
-    mobileImage:
-      mobile,
-    targetSection: "team",
-  },
-  {
-    id: 4,
-    desktopImage:
-      "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Group%20179-KYso8EiLDRqzfN66Tac9F7rZ5KOgNM.png",
-    mobileImage:
-      "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Group%20179%20(1)-XX5SJRUOJcK6el4fLwWIW4vni4pOUK.png",
-    targetSection: "team",
+    targetSection: "specialties"
   },
 ]
 
@@ -68,10 +156,9 @@ export default function HeroCarousel() {
   return (
     <Swiper
       modules={[Navigation, Pagination, Autoplay]}
-      navigation
       pagination={{ clickable: true }}
       autoplay={{ delay: 5000 }}
-      className="w-full h-screen"
+      className="w-full h-screen [&_.swiper-pagination-bullet]:bg-white [&_.swiper-pagination-bullet-active]:bg-green-500"
     >
       {slides.map((slide) => (
         <SwiperSlide key={slide.id}>
